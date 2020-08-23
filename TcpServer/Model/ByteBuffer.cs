47ed4c5ab755e3916ec5381859c6a623a34ce942
jian <my123jian@gmal.com>
2020-08-23 21:25:40 +0800

    test

---
+++ TcpServer/Model/ByteBuffer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace TcpServer.Model
+{
+    public class ByteBuffer:IDisposable
+    {
+        private System.IO.MemoryStream theBufferStream = null;
+        public ByteBuffer()
+        {
+            
+        }
+        public void Append(Byte[] data,int start,int end)
+        {
+            if (theBufferStream == null)
+            {
+                theBufferStream = new System.IO.MemoryStream();
+            }
+            for (var i = start; i < end; i++){
+                theBufferStream.WriteByte(data[i]);
+            }
+           
+        }
+
+        public void Dispose()
+        {
+            if (theBufferStream != null)
+            {
+                theBufferStream.Close();
+                theBufferStream.Dispose();
+            }
+        }
+
+        public byte[] ToBytes()
+        {
+            var theBytes = this.theBufferStream.ToArray();
+            this.theBufferStream = null;
+            return theBytes;
+        }
+    }
+}
---
+++ TcpServer/Service/ServerClientSocket.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Sockets;
+using TcpServer.Model;
+
+namespace TcpServer.Service
+{
+    public delegate void OnDataReceived(ServerClientSocket sender,byte[]Data);
+    public class ServerClientSocket : IDisposable
+    {
+        public Socket Socket { get; set; }
+        private ByteBuffer theDataBuffer;
+        protected byte[] Buffer;
+        public event OnDataReceived OnDataReceived;
+        public ServerClientSocket()
+        {
+            this.Buffer = new byte[1024];
+            this.theDataBuffer = new ByteBuffer();
+        }
+
+        protected virtual void DoRecv()
+        {
+            this.Socket.BeginReceive(Buffer, 0, 1024, SocketFlags.None, ReceiveCallBack, 0);
+        }
+
+        protected virtual void ReceiveCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                int count = this.Socket.EndReceive(ar);
+                if (count >= 0)
+                {
+                    this.theDataBuffer.Append(this.Buffer, 0, count);
+                    this.DoRecv();
+                }
+                else
+                {
+                    this.OnReceived(this.theDataBuffer.ToBytes());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        protected virtual void OnReceived(byte[] Data)
+        {
+            this.OnDataReceived(this, Data);
+        }
+
+        public void Dispose()
+        {
+            if (this.Socket != null) {
+                this.Socket.Close();
+            }
+            this.theDataBuffer.Dispose();
+            
+        }
+    }
+}
